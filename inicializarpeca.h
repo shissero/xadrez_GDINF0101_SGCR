6095960cdd6e7566e9cd098b0c6ed976d836f8ef
R1cardo4lexandre <ricardoalexandre7700@gmail.com>
2024-10-02 18:03:15 -0300

    Implementação do Inicializador de peças

--- inicializarpeca.h
+++ inicializarpeca.h
@@ -2,6 +2,6 @@
 #define inicializarpeca_h
 #include "peca.h"
 
-void inicializarPeca (PtrPeca*); 
+void inicializarPeca(PtrPeca *ptrpeca, char t_peca[], int color, int coord[]);
 
 #endif
--- main.c
+++ main.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 #include "peca.h"
 #include "inicializarpeca.h"
-int main(){
+#include "inicializarpeca.c"
+
+int main() {
     
     Peca peca;
-    PtrPeca *ptrpeca;       
-    inicializarPeca (ptrpeca);
-
-printf("%s", peca.tipoPeca);
+    PtrPeca ptrpeca = &peca;       
+    
+    char tipo[] = "Cavalo";
+    int cor = 0; 
+    int posicao[] = {1, 0};
+    
+    inicializarPeca(&ptrpeca, tipo, cor, posicao);
+    
+    printf("%s\n", peca.tipoPeca);
+    printf("cor %d\n", peca.cor);
+    printf("coord (%d, %d)\n", peca.posicao[0], peca.posicao[1]);
      
 }
