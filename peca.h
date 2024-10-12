5d8468caa04cf0c6351cf7e9c52a9c3c3385f66e
Cícero Augusto <cicero1995augustoo@gmail.com>
2024-10-12 12:11:57 -0300

    Atualização de peca.h
    Instruções com mudanças a serem feitas

--- peca.h
+++ peca.h
@@ -1,8 +1,10 @@
 #ifndef PECA_H_
 #define PECA_H_
 
+// TODO : Reescreva essa declaração. Não use typedef nem defina um tipo de ponteiro para peca
 typedef struct {
 
+  // TODO : É necessário especificar constantes para cada tipo de peça. Você pode usar macros ou uma enumeração
   int tipoPeca;
   int cor;
   int posicao [2];
