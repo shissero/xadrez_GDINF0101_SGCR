88995a72a14b1cc33d1cabd7974b52963d2cea97
R1cardo4lexandre <ricardoalexandre7700@gmail.com>
2024-10-12 16:26:33 -0300

    atualização da função movimentos_cardeais.c

---
+++ movimentos_cardeais.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "peca.h"
+#include "movimentosCardeais.h"
+
+ //função para gerar movimentos cardeais
+// Função para gerar movimentos cardeais
+void movimentosCardeais(int coluna, int linha) {
+
+    //movimentos cardeais
+    
+    //norte
+    for (int i = 1; linha + i; i++)
+    //sul
+    for (int i = 1; linha - i; i++)
+    //leste
+    for (int i = 1; coluna + i; i++)
+    //oeste
+    for (int i = 1; coluna - i; i++)  
+    
+}
