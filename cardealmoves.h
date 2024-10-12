2104abe95cbedaabfa2840e0e34931fc84c21508
Cícero Augusto <cicero1995augustoo@gmail.com>
2024-10-11 21:47:42 -0300

    Atualização de cardealmoves.h
    Adicionei mais comentários com melhoras no código

--- cardealmoves.h
+++ cardealmoves.h
@@ -1,10 +1,11 @@
-// TODO : Os header guarders têm que estar em caixa alta
+// TODO : Os header guarders têm que estar em caixa alta, as palavras separadas por underlines
 #ifndef cardealmoves_h_
 #define cardealmoves_h_
 
 #include"peca.h"
 
 // TODO : O nome dessa função deveria estar em português e em camel case
+// TODO : O nome do arquivo deve ser o mesmo função, então não esqueça de renome-álo também
 void cardealmoves (int, int, int movimentos[8][2]);
 
 #endif
