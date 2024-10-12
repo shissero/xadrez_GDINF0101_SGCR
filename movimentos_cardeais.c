57b4bf96bb9576967ae070bc27e508b2895fcf98
R1cardo4lexandre <ricardoalexandre7700@gmail.com>
2024-10-12 17:50:54 -0300

    atualização da função movimentos_cardeais

--- movimentos_cardeais.c
+++ movimentos_cardeais.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 #include "peca.h"
-#include "movimentosCardeais.h"
+#include "movimentos_cardeais.h"
 
- //função para gerar movimentos cardeais
 // Função para gerar movimentos cardeais
 void movimentosCardeais(int coluna, int linha) {
-
-    //movimentos cardeais
-    
-    //norte
-    for (int i = 1; linha + i; i++)
-    //sul
-    for (int i = 1; linha - i; i++)
-    //leste
-    for (int i = 1; coluna + i; i++)
-    //oeste
-    for (int i = 1; coluna - i; i++)  
+  
+  //movimentos cardeais
+  //norte
+  for (int i = 1; linha + i <= 8; i++) {
+    printf("(%d, %d)\n", coluna, linha);
+  }
+  
+  //sul
+  for (int i = 1; linha - i >= 1; i++) {
+    printf("(%d, %d)\n", coluna, linha);
+  }
+  
+  //leste
+  for (int i = 1; coluna + i <= 8; i++) {
+    printf("(%d, %d)\n", coluna, linha);
+  }
+  
+  //oeste
+  for (int i = 1; coluna - i >= 1; i++) {
+    printf("(%d, %d)\n", coluna, linha);
+  }  
+  
     
 }
