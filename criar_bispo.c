f3cd8776f09012d5e15d9661f8e8b0a39ec3f82a
Cícero Augusto <cicero1995augustoo@gmail.com>
2024-10-12 22:33:43 -0300

    Atualização e renomeação de bispo.c to criar_bispo.c

---
+++ criar_bispo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "peca.h"
+#include "inicializarpeca.h"
+#include "colateralmoves.h"
+#include "bispo.h"
+
+// TODO: Adequar essa função à nova implementação de inicializarPeca
+void criarBispo(Peca *bispo, int cor, int coluna, int linha) {
+
+    
+    inicializarPeca(bispo, tipoPeca, cor, coord);
+}
