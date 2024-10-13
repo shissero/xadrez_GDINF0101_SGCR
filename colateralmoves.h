11c974351f977ac40ba64f06a524cd4108d7717a
R1cardo4lexandre <ricardoalexandre7700@gmail.com>
2024-10-12 21:32:06 -0300

    atualização da função de gerar movimentos colaterais já testada

--- colateralmoves.h
+++
@@ -1,8 +0,0 @@
-#ifndef colateralmoves_h_
-#define colateralmoves_h_
-
-#include"peca.h"
-
-void colateralmoves (int, int, int movimentos[8][2]);
-
-#endif
---
+++ movimentos_colaterais.h
@@ -0,0 +1,8 @@
+#ifndef MOVIMENTOS_COLATEAIS_H_
+#define MOVIMENTOS_COLATERAIS_H_
+
+#include"peca.h"
+
+void movimentosColaterais (int, int);
+
+#endif
