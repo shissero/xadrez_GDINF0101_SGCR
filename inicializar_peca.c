98ec601ff5a6f031e45a136da6ca2d2d49ae5435
Cícero Augusto <cicero1995augustoo@gmail.com>
2024-10-12 21:56:54 -0300

    Atualização e renomeação de inicializarpeca.c to inicializar_peca.c

--- inicializar_peca.c
+++ inicializar_peca.c
@@ -3,8 +3,11 @@
 #include "inicializarpeca.h"
 #include "peca.h"
 
+// TODO: É necessário mudar os tipos dos parâmetros para se adequar à nova forma da struct Peca
+// TODO: As coordenadas devem ser passsadas como dois parâmetros diferentes
 void inicializarPeca (PtrPeca *ptrpeca, char t_peca[], int color, int coord[]) {
 
+    // TODO: É necessário validar o tipo de peça recebido, para garantir que um tipo inválido não será passado
     strcpy((*ptrpeca)->tipoPeca, t_peca); 
     (*ptrpeca)->cor = color;                 
     (*ptrpeca)->posicao[0] = coord[0];
